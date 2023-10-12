d40b71393b5df0190cfa83fa6324716ae25a1716
jkimball54 <jeremyck54@gmail.com>
2023-10-12 10:13:35 -0600

    finished error handling portion

--- RecordCollection/Controllers/AlbumsAPIController.cs
+++ RecordCollection/Controllers/AlbumsAPIController.cs
@@ -9,10 +9,12 @@ namespace RecordCollection.Controllers
     public class AlbumsAPIController : ControllerBase
     {
         private readonly RecordCollectionContext _context;
+		private readonly Serilog.ILogger _logger;
 
-        public AlbumsAPIController(RecordCollectionContext context, Serilog.ILogger logger)
+		public AlbumsAPIController(RecordCollectionContext context, Serilog.ILogger logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public IActionResult GetAll()
@@ -22,16 +24,34 @@ namespace RecordCollection.Controllers
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetOne(int id)
+        public IActionResult GetOne(int? id)
         {
+            if(id == null)
+            {
+                _logger.Warning("AlbumsApiAPI GetOne - GET Failed: Id was null");
+                return new JsonResult(null);
+            }
             var album = _context.Albums.FirstOrDefault(a => a.Id == id);
+            if(album == null)
+            {
+				_logger.Warning("AlbumsApiAPI GetOne - GET Failed: Album was null");
+				return new JsonResult(null);
+			}
             return new JsonResult(album);
         }
 
         [HttpDelete("{id}")]
-        public void DeleteOne(int id)
+        public void DeleteOne(int? id)
         {
+            if(id == null)
+            {
+                _logger.Warning("AlbumsApiAPI DeleteOne - Delete Failed: Id was null");
+            }
             var album = _context.Albums.FirstOrDefault(a => a.Id == id);
+            if(album == null)
+            {
+				_logger.Warning("AlbumsApiAPI DeleteOne - Delete Failed: Album was null");
+			}
             _context.Albums.Remove(album);
             _context.SaveChanges();
         }
--- RecordCollection/Models/Album.cs
+++ RecordCollection/Models/Album.cs
@@ -1,10 +1,17 @@
-﻿namespace RecordCollection.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecordCollection.Models
 {
     public class Album
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required!")]
+        [StringLength(50, ErrorMessage = "Title cannot exceed 50 characters")]
         public string Title { get; set; }
-        public string Artist { get; set; }
+		[Required(ErrorMessage = "Artist is required!")]
+		[StringLength(50, ErrorMessage = "Artist cannot exceed 50 characters")]
+		public string Artist { get; set; }
+        [Required(ErrorMessage = "Rating is required!")]
         public int Rating { get; set; }
     }
 }
